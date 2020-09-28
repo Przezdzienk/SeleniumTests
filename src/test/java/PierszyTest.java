adfc25a5884ab5db7698461498e32a4a0bd5794c
BartlomiejPrzezdzienk <przezdzienk115@wp.pl>
2020-09-28 13:27:13 +0200

    testowanie ciag dalszy

--- src/test/java/PierszyTest.java
+++ src/test/java/PierszyTest.java
@@ -1,14 +1,34 @@
-import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class PierszyTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void driverSet(){
+        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
+        driver= new FirefoxDriver();
+    }
+    @AfterEach
+    public void stopDriver(){
+        driver.quit();
+    }
     @Test
     public void firstTest(){
-        System.setProperty("webdriver.gecko.driver","src/main/resources/geckodriver.exe");
-        WebDriver driver= new FirefoxDriver();
-        driver.navigate().to("http://automationpractice.com/index.php");
-        driver.quit();
+        driver.navigate().to("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna");
+        driver.navigate().to("https://manganelo.com/");
+        driver.navigate().back();
+
+        Assert.assertEquals("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna",driver.getCurrentUrl());
+
+        driver.navigate().forward();
+
+        Assert.assertEquals("https://manganelo.com/",driver.getCurrentUrl());
 
     }
 }
